9a580ae4d438489a7afd42ef9ca2f0b8c7d29da9
Frenken, S.H. (Sander) <>
2024-09-03 22:02:47 +0200

    Disable swiftlint because of xcode cloud integration breaks

--- Package.swift
+++ Package.swift
@@ -18,8 +18,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MSKTiled",
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]),
+            name: "MSKTiled"//,
+//            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+        ),
         .testTarget(
             name: "MSKTiledTests",
             dependencies: ["MSKTiled"]),
